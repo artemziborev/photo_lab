78260007fd876dd692d129c2c4867b4a03a21bfc
Artem Ziborev <ziborev@akticom.ru>
2020-10-23 14:20:15 +0400

    Settings

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
 ]
-
+ix
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
